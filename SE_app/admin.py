3b21501cfb54c58aa0ef441a96c7c7b797c8113e
Eevee <richardkuo2002@gmail.com>
2023-06-02 13:41:32 +0800

    Update admin.py

--- SE_app/admin.py
+++ SE_app/admin.py
@@ -1,12 +1,3 @@
 from django.contrib import admin
-from models import *
-
 
 # Register your models here.
-
-admin.site.register(Branch)
-admin.site.register(Sale)
-admin.site.register(Profit)
-admin.site.register(Salesperson)
-admin.site.register(Customer)
-admin.site.register(CustomerProgress)
