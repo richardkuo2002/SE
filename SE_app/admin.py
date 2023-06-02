a11c3221a218be9b9a00c255ffbe07f017dee774
Eevee <richardkuo2002@gmail.com>
2023-06-02 13:33:30 +0800

    Update admin.py

--- SE_app/admin.py
+++ SE_app/admin.py
@@ -5,8 +5,8 @@ from .models import *
 # Register your models here.
 
 admin.site.register(Branch)
-# admin.site.register(Sale)
-# admin.site.register(Profit)
-# admin.site.register(Salesperson)
-# admin.site.register(Customer)
-# admin.site.register(CustomerProgress)
+admin.site.register(Sale)
+admin.site.register(Profit)
+admin.site.register(Salesperson)
+admin.site.register(Customer)
+admin.site.register(CustomerProgress)
